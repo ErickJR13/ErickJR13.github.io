97a7563056e1ea5393086844caf940b3856ec765
Erick Reyes <ErickJR13@users.noreply.github.com>
2018-04-17 10:08:24 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
 console.log('This would be the main JS file.');
+
+<HEAD>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116369119-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-116369119-1');
+</script>
+</HEAD>
